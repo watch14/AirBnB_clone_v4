8108eba2934d5b295d7972617527ac2233e0f6a5
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-23 10:39:36 +0100

    fast_push

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3xx
 """ index """
 
+import models
+from models import storage
+from models.base_model import BaseModel
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -9,3 +12,21 @@ from flask import jsonify
 def return_status():
     """ return status"""
     return jsonify(status="ok")
+
+
+@app_views.route("/stats", strict_slashes=False)
+def counter(self):
+    """ counter """
+    all_c = {
+                "amenities": Amenity,
+                "cities": City,
+                "places": Place,
+                "reviews": Review,
+                "states": State,
+                "users": User
+            }
+
+    for key in all_c:
+        all_c[key] = storage.count(all_c[key])
+
+    return jsonify(all_c)
