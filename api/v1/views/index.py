fcb284f4af898ece5df5c7c28c3472c124ee9a5c
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-25 14:04:08 +0000

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
 """ index """
-
-from models import storage
 from api.v1.views import app_views
 from flask import jsonify
-from models.base_model import BaseModel
-from models.amenity import Amenity
+from models import storage
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models.amenity import Amenity
 
 
 @app_views.route('/status')
 def return_status():
     """ return status"""
-    return jsonify({'status': 'ok'})
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route("/stats", strict_slashes=False)
+@app_views.route('/stats')
 def counter():
     """ counter """
     all_c = {
-            "amenities": storage.count(Amenity),
-            "cities": storage.count(City),
-            "places": storage.count(Place),
-            "reviews": storage.count(Review),
-            "states": storage.count(State),
-            "users": storage.count(User),
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
             }
     return jsonify(all_c)
