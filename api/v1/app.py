04693422cc0f041cea9a2ebd5236d5600a92710d
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-25 13:50:56 +0000

    Updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,12 +25,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST")
-    port = os.getenv("HBNB_API_PORT")
-
-    if not host:
-        host = "0.0.0.0"
-    if not port:
-        port = "5000"
-
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
