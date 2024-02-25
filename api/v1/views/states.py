698efee94dca6ce55f8f0e2025230fdb4603c590
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-25 12:00:48 +0100

    fast_push

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,7 @@
 
 from api.v1.views import app_views
 from flask import abort, jsonify, Flask, request
-from models import storage
+from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
